5d2ef1c8e2f1950e88afe690d65f50a78efeb4b0
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-04-30 23:43:41 +0700

    Edit minor html changes

--- src/main/java/com/budgetku/controller/MainController.java
+++ src/main/java/com/budgetku/controller/MainController.java
@@ -22,12 +22,13 @@ public class MainController {
 
     @GetMapping("/list-budget")
     public String listBudget(Model model) {
+        // Should contain Budget model objects, not yet made
         List<String[]> dummyBudget = new ArrayList<>();
-        dummyBudget.add(new String[]{"Makanan", "50.000", "Budget makanan bulanan"});
+        dummyBudget.add(new String[]{"Makanan", "200.000", "Budget makanan bulanan"});
         dummyBudget.add(new String[]{"Pulsa", "50.000", "Budget pulsa bulanan"});
-        dummyBudget.add(new String[]{"Belanja", "50.000", "Budget belanja online bulanan"});
-        dummyBudget.add(new String[]{"Obat", "50.000", "Budget obat bulanan"});
-        dummyBudget.add(new String[]{"Transport", "50.000", "Budget transport bulanan"});
+        dummyBudget.add(new String[]{"Belanja", "100.000", "Budget belanja online bulanan"});
+        dummyBudget.add(new String[]{"Obat", "30.000", "Budget obat bulanan"});
+        dummyBudget.add(new String[]{"Transport", "100.000", "Budget transport bulanan"});
         model.addAttribute("budgetList", dummyBudget);
         return "list-budget";
     }
