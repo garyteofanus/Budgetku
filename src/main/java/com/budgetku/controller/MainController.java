f4cd773fc8e007181ad6cba3c23eba8a7992176b
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2021-04-30 23:18:04 +0700

    Add controller

--- src/main/java/com/budgetku/controller/MainController.java
+++ src/main/java/com/budgetku/controller/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/budget-limiter")
+    public String budgetLimiter() {
+        return "budgetLimiter/index.html";
+    }
 }
