36624dbc78ad89778943475c519cc38737f2635d
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-04-30 23:14:49 +0700

    Init list-budget page with dummy data

--- src/main/java/com/budgetku/controller/MainController.java
+++ src/main/java/com/budgetku/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.budgetku.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -14,5 +18,17 @@ public class MainController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/list-budget")
+    public String listBudget(Model model) {
+        List<String[]> dummyBudget = new ArrayList<>();
+        dummyBudget.add(new String[]{"Makanan", "50.000", "Budget makanan bulanan"});
+        dummyBudget.add(new String[]{"Pulsa", "50.000", "Budget pulsa bulanan"});
+        dummyBudget.add(new String[]{"Belanja", "50.000", "Budget belanja online bulanan"});
+        dummyBudget.add(new String[]{"Obat", "50.000", "Budget obat bulanan"});
+        dummyBudget.add(new String[]{"Transport", "50.000", "Budget transport bulanan"});
+        model.addAttribute("budgetList", dummyBudget);
+        return "list-budget";
     }
 }
