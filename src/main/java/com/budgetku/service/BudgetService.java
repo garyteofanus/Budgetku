27010bd554ff75213b358bb99cf770335d8e1449
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2021-05-07 21:31:27 +0700

    Add skeleton service for budget

--- src/main/java/com/budgetku/service/BudgetService.java
+++ src/main/java/com/budgetku/service/BudgetService.java
@@ -4,6 +4,9 @@ import com.budgetku.model.Budget;
 
 public interface BudgetService {
     Iterable<Budget> getListBudget();
+
+    void createBudget(Budget budget);
+
     Budget getBudgetById(int id);
     Budget updateBudget(int id);
     void deleteBudgetById(int id);
