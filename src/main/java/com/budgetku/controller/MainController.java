509fc6795a4a55ec431ee7288dba84a3678967e9
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2021-05-07 21:30:02 +0700

    Separate budget controller from main controller

--- src/main/java/com/budgetku/controller/MainController.java
+++ src/main/java/com/budgetku/controller/MainController.java
@@ -1,11 +1,8 @@
 package com.budgetku.controller;
 
-import com.budgetku.model.Budget;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -18,17 +15,6 @@ public class MainController {
     @GetMapping("/")
     public String home() {
         return "index";
-    }
-
-    @GetMapping("/budget-limiter")
-    public String budgetLimiter(Model model) {
-        model.addAttribute("budget", new Budget());
-        return "budgetLimiter/index.html";
-    }
-
-    @PostMapping("/budget-limiter/add-budget")
-    public String addBudget(@ModelAttribute Budget budget) {
-        return "redirect:/budget-limiter?success";
     }
 
     @GetMapping("/list-budget")
