155d97dd259977e357ea1654a4a70e7e67a9d1ce
Nabila Dita Putri <nabiladita0501@gmail.com>
2021-05-07 21:46:59 +0700

    fix controller

--- src/main/java/com/budgetku/controller/KategoriController.java
+++ src/main/java/com/budgetku/controller/KategoriController.java
@@ -5,8 +5,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import com.budgetku.service.KategoriService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
