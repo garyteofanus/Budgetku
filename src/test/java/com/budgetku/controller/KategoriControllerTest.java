486da96de3472f2d6f9ac2f5bed567e4fb0b43d6
Nabila Dita Putri <nabiladita0501@gmail.com>
2021-05-21 23:45:38 +0700

    add test for controller

---
+++ src/test/java/com/budgetku/controller/KategoriControllerTest.java
@@ -0,0 +1,50 @@
+package com.budgetku.controller;
+
+import com.budgetku.service.KategoriService;
+import com.budgetku.service.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+
+@WebMvcTest(controllers = KategoriController.class)
+public class KategoriControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    KategoriService kategoriService;
+
+    @Test
+    public void testGetBudgetStatusShouldRedirectWhenNotLoggedIn() throws Exception {
+        this.mockMvc.perform(get("/kategori/")).andDo(print()).andExpect(status().isFound());
+    }
+
+    @Test
+    @WithMockUser(username = "usertest@gmail.com", password = "dummypassword", roles = "USER")
+    public void whenCreateKategoriUrlShouldCallKategoriService() throws Exception{ ;
+        mockMvc.perform(get("/kategori"))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("getKategori"))
+                .andExpect(model().attributeExists("kategori"))
+                .andExpect(view().name("kategori"));
+        verify(kategoriService, times(1)).getListKategoriByUser("usertest@gmail.com");
+    }
+
+}
+
