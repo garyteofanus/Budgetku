77545002b84987c584d42860047be841cdc2db22
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2021-06-04 23:50:34 +0700

    Implement method body to accept JSON post request

--- src/main/java/com/budgetku/controller/BudgetController.java
+++ src/main/java/com/budgetku/controller/BudgetController.java
@@ -1,20 +1,20 @@
 package com.budgetku.controller;
 
-import com.budgetku.model.Budget;
 import com.budgetku.service.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/budget")
 public class BudgetController {
@@ -23,12 +23,23 @@ public class BudgetController {
     private BudgetService budgetService;
 
     @CrossOrigin(origins = "http://localhost:8080")
-    @PostMapping(path = "/create", produces = {"application/json"})
+    @PostMapping(path = "/create/{email}", produces = {
+        "application/json"}, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseBody
-    public ResponseEntity createBudget(@RequestBody Budget budget) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String userEmail = authentication.getName();
-        return ResponseEntity.ok(budgetService.createBudget(budget, userEmail));
+    public String createBudget(
+        String payload,
+        @PathVariable("email") String email) throws Exception {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String userEmail = authentication.getName();
+        System.out.println(payload);
+        System.out.println(email);
+        return "redirect:/feedback/success";
+        // return ResponseEntity.ok(budgetService.createBudget(budget, email));
+    }
+
+    @GetMapping("/feedback/success")
+    public ResponseEntity<String> getSuccess() {
+        return new ResponseEntity<String>("Thank you for submitting feedback.", HttpStatus.OK);
     }
 
     @GetMapping(path = "/list/{userId}", produces = {"application/json"})
